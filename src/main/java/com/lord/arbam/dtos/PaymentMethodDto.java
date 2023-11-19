4905801653ad4a381566306abfaf69e36bd29987
Fausto Arduino <faustard@gmail.com>
2023-11-19 14:58:22 -0400

    .

---
+++ src/main/java/com/lord/arbam/dtos/PaymentMethodDto.java
@@ -0,0 +1,15 @@
+package com.lord.arbam.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PaymentMethodDto {
+
+	
+	private Long id;
+	
+
+	private String paymentMethod;
+}
