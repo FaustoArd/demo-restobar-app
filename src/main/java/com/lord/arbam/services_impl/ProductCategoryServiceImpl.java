6a44c0004007a28fa9624685fdc2126419fbeee1
Fausto Arduino <faustard@gmail.com>
2023-11-19 01:55:07 -0400

    .

--- src/main/java/com/lord/arbam/services_impl/ProductCategoryServiceImpl.java
+++ src/main/java/com/lord/arbam/services_impl/ProductCategoryServiceImpl.java
@@ -2,6 +2,8 @@ package com.lord.arbam.services_impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.lord.arbam.exceptions.ItemNotFoundException;
@@ -15,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class  ProductCategoryServiceImpl implements CategoryService<ProductCategory> {
 
+	private static Logger log = LoggerFactory.getLogger(ProductCategoryServiceImpl.class);
+	
 	@Autowired
 	private final ProductCategoryRepository productCategoryRepository;
 
@@ -26,6 +30,7 @@ public class  ProductCategoryServiceImpl implements CategoryService<ProductCateg
 
 	@Override
 	public ProductCategory saveCategory(ProductCategory category) {
+		log.info("Creando nueva categoria");
 		return productCategoryRepository.save(category);
 	}
 
