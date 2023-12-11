49e3cfdbccfe785b01bf5653622d7f3047659a51
Fausto Arduino <faustard@gmail.com>
2023-12-11 00:17:38 -0400

    .

--- target/generated-sources/annotations/com/lord/arbam/mapper/EmployeeMapperImpl.java
+++ target/generated-sources/annotations/com/lord/arbam/mapper/EmployeeMapperImpl.java
@@ -11,7 +11,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-06T21:11:33-0400",
+    date = "2023-12-08T14:23:57-0400",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.34.0.v20230523-1233, environment: Java 17.0.7 (Oracle Corporation)"
 )
 public class EmployeeMapperImpl implements EmployeeMapper {
