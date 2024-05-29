13b0372686d9773fe6db7d56bf03de38c8d75da7
Fausto Arduino <faustard@gmail.com>
2024-05-29 19:19:09 -0300

    .

--- target/generated-sources/annotations/com/lord/arbam/mapper/RestoTableOrderClosedMapperImpl.java
+++ target/generated-sources/annotations/com/lord/arbam/mapper/RestoTableOrderClosedMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-05-29T16:28:51-0300",
+    date = "2024-05-29T18:20:07-0300",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.34.0.v20230523-1233, environment: Java 17.0.7 (Oracle Corporation)"
 )
 public class RestoTableOrderClosedMapperImpl implements RestoTableOrderClosedMapper {
